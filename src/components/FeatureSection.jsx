513c55a35903faf09518fb4c4b8df82afa70062c
EsraTemiz <esratemizz@hotmail.com>
2026-07-23 22:49:30 +0300

    FetureSection eklendi

---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FeatureSection = () => {
+  return (
+    
+    <div className=''>
+      <h2 className='text-3xl text-olive-700 font-bold py-3'> Neden Daily Gemetry?</h2>
+      <p className='text-2xl text-olive-800 py-5'> Geometriyi yalnızca çözmeyi değil, doğru düşünmeyi hedefleyen bir öğrenme deneyimi.</p>
+    </div>
+  
+  )
+}
+
+export default FeatureSection
+
