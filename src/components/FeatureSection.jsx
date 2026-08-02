e913b0cd0fe43933ce0c1990ebd954593feca1f0
EsraTemiz <esratemizz@hotmail.com>
2026-08-02 23:57:09 +0300

    feature section da düzenleme

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -8,20 +8,20 @@ const FeatureSection = () => {
   let smallCard=[
 {  icon:<GiBrain/>,
    id:1,
-  title:"Çözüm Steratejileri",
+  title:"Çözüm Stratejileri",
   description:"Sadece cevabı değil düşünme sürecini öğren"
 },
-{ icon:<FaRegLightbulb/>,
+{ icon:<CiWarning/>,
   id:2,
   title:"Sık Yapılan Hatalar",
-  description:"yaptığın Hataları diğer öğrencilerle karşılaştır"
+  description:"Yaptığın hataları diğer öğrencilerle karşılaştır"
 },
 { icon:<FaFireAlt/>,
   id:3,
   title:"Günün Sorusu",
-  description:"Her gün günvel bir soruyla kendini test et"
+  description:"Her gün güncel bir soruyla kendini test et"
 },
-{ icon:<CiWarning/>,
+{ icon:<FaRegLightbulb/>,
   id:4,
   title:"Günün İpucu",
   description:"Herhangi bir konuda soru çözerken en çok neye dikkat etmelisin"
@@ -29,18 +29,18 @@ const FeatureSection = () => {
   ]
   return (
     
-    <div className=''>
-      <h2 className='text-3xl text-olive-700 font-bold py-3'> Neden Daily Geometry?</h2>
-      <p className='text-xl text-olive-800 py-3'> Geometriyi yalnızca çözmeyi değil, doğru düşünmeyi hedefleyen bir öğrenme deneyimi.</p>
+    <div className='text-center max-w-3xl mx-auto'>
+      <h2 className='text-4xl text-olive-700 font-bold py-3'> Neden Daily Geometry?</h2>
+      <p className='text-xl text-olive-800 mt-4'> Geometriyi yalnızca çözmeyi değil, doğru düşünmeyi hedefleyen bir öğrenme deneyimi.</p>
      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 py-5 '>
         
           {smallCard.map((special) => (
-           <div key={special.id} className='border-olive-500 shadow-lg bg-white rounded-2xl p-3 '>
-            <span className='text-xl text-mustard-500'>{special.icon}</span>
-            <h2 className='text-xl text-dark font-bold'>
+           <div key={special.id} className='h-full flex flex-col border border-olive-200 bg-white rounded-2xl p-5 shadow-md transition-all duration-300 hover:-translate-y-2 hover:shadow-xl '>
+            <span className='text-4xl text-mustard-500 mb-4 inline-block'>{special.icon}</span>
+            <h2 className='text-xl font-bold text-dark mt-2 mb-3'>
               {special.title}
              </h2>
-              <p className='text-olive-800 ' >
+              <p className='text-olive-800 leading-7' >
                   {special.description}
               </p>
              </div>
